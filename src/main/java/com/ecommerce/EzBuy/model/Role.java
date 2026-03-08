3bffafddf7a72ae83b2880a0deb3dbf87f6a8cf9
AmolAnshuman <amolanshuman01@gmail.com>
2026-03-08 23:59:47 +0530

    feat: create Role entity with enum-based role names

---
+++ src/main/java/com/ecommerce/EzBuy/model/Role.java
@@ -0,0 +1,25 @@
+package com.ecommerce.EzBuy.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING) //by default enum is persisted as integer, we are changing it to string
+    @Column(length = 20,name = "role_name")
+    private AppRole roleName ;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+
+}
