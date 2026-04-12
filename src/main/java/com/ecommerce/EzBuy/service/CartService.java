aaf624cdd3ff52602276d45866628f75ffe8b2ef
AmolAnshuman <amolanshuman01@gmail.com>
2026-04-12 17:51:21 +0530

    API to get all carts present in the repository.

--- src/main/java/com/ecommerce/EzBuy/service/CartService.java
+++ src/main/java/com/ecommerce/EzBuy/service/CartService.java
@@ -2,6 +2,10 @@ package com.ecommerce.EzBuy.service;
 
 import com.ecommerce.EzBuy.payload.CartDTO;
 
+import java.util.List;
+
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity) ;
+
+    List<CartDTO> getAllCarts();
 }
--- src/main/java/com/ecommerce/EzBuy/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/EzBuy/service/CartServiceImpl.java
@@ -81,6 +81,23 @@ public class CartServiceImpl implements CartService{
         return cartDTO;
     }
 
+    @Override
+    public List<CartDTO> getAllCarts() {
+        List<Cart>carts = cartRepository.findAll();
+        if(carts.isEmpty()){
+            throw new APIException("no cart exists");
+        }
+        List<CartDTO> cartDTOs = carts.stream().map(cart -> {
+            CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+            List<ProductDTO> productDTOS = cart.getCartItems().stream()
+                    .map(p -> modelMapper.map(p.getProduct(),ProductDTO.class))
+                    .collect(Collectors.toList());
+            cartDTO.setProducts(productDTOS);
+            return cartDTO;
+        }).collect(Collectors.toList());
+        return cartDTOs;
+    }
+
     private Cart createCart() {
         Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
         if(userCart != null) {
