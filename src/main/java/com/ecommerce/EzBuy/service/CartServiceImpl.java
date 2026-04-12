bfffa2b2ed67d8a0a08cda75a1700548512ba8f7
AmolAnshuman <amolanshuman01@gmail.com>
2026-04-13 02:43:39 +0530

    feat: update product quantity in a User Cart

--- src/main/java/com/ecommerce/EzBuy/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/EzBuy/service/CartServiceImpl.java
@@ -11,6 +11,7 @@ import com.ecommerce.EzBuy.repositories.CartItemRepository;
 import com.ecommerce.EzBuy.repositories.CartRepository;
 import com.ecommerce.EzBuy.repositories.ProductRepository;
 import com.ecommerce.EzBuy.util.AuthUtil;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -111,6 +112,49 @@ public class CartServiceImpl implements CartService{
                 .map(p -> modelMapper.map(p.getProduct(),ProductDTO.class))
                 .collect(Collectors.toList());
         cartDTO.setProducts(products);
+        return cartDTO;
+    }
+
+    @Transactional
+    @Override
+    public CartDTO updateProductQuantityInCart(Long productId, Integer quantity) {
+
+        String emailId = authUtil.loggedInEmail();
+        Cart userCart = cartRepository.findCartByEmail(emailId);
+        Long cartId = userCart.getCartId();
+        Cart cart = cartRepository.findById(cartId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart", "cartId", cartId));
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
+        if(product.getQuantity() == 0){
+            throw new APIException(product.getProductName() + " is not in stock");
+        }
+        if(product.getQuantity() < quantity){
+            throw new APIException("Please enter less than or equal to " + product.getQuantity() + "quantity");
+        }
+        CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(cartId,productId);
+        if(cartItem == null){
+            throw new APIException("Product" + product.getProductName() + " is not in the cart");
+        }
+        cartItem.setProductPrice(product.getSpecialPrice());
+        cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        cartItem.setDiscount(product.getDiscount());
+        cart.setTotalPrice(cart.getTotalPrice() + product.getSpecialPrice()*quantity);
+        cartRepository.save(cart);
+        CartItem updatedItem = cartItemRepository.save(cartItem);
+        if(updatedItem.getQuantity() == 0){
+            cartItemRepository.deleteById(updatedItem.getCartItemId());
+        }
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+        List<CartItem> cartItems = cart.getCartItems();
+        Stream<ProductDTO> productDTOStream = cartItems.stream().map(item ->{
+            ProductDTO prd = modelMapper.map(item.getProduct(),ProductDTO.class);
+            prd.setQuantity(item.getQuantity());
+            return prd;
+        });
+        cartDTO.setProducts(productDTOStream.collect(Collectors.toList()));
         return cartDTO;
     }
 
